acb299126535b06f96037fd14176ae4d5d90dc9c
arbia123 <lassoued.arbia92@gmail.com>
2019-10-11 22:36:33 +0100

    added Blockchain

---
+++ contracts/election.sol
@@ -0,0 +1,48 @@
+pragma  solidity ^0.5.0;
+
+contract Election{
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    // Read/write candidates
+    mapping(uint => Candidate) public candidates;
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+        addCandidate("Candidate 3");
+        addCandidate("Candidate 4");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+    event votedEvent (
+        uint indexed _candidateId
+    );
+    function vote (uint _candidateId) public {
+    // require that they haven't voted before
+    require(!voters[msg.sender]);
+
+    // require a valid candidate
+    require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+    // record that voter has voted
+    voters[msg.sender] = true;
+
+    // update candidate vote Count
+    candidates[_candidateId].voteCount ++;
+
+    // trigger voted event
+   emit  votedEvent(_candidateId);
+}
+
+
+}
